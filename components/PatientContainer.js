53848f1e8840440ef49f19a21e38a948077f58e9
nvanlonden <nielsvlonden@gmail.com>
2022-06-30 13:46:14 -0400

    create more details in patient tabs

--- components/PatientContainer.js
+++ components/PatientContainer.js
@@ -250,36 +250,7 @@ const directory = {
     },
   ],
 }
-const team = [
-  {
-    name: 'Leslie Alexander',
-    handle: 'lesliealexander',
-    role: 'Co-Founder / CEO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Michael Foster',
-    handle: 'michaelfoster',
-    role: 'Co-Founder / CTO',
-    imageUrl:
-      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Dries Vincent',
-    handle: 'driesvincent',
-    role: 'Manager, Business Relations',
-    imageUrl:
-      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-  {
-    name: 'Lindsay Walton',
-    handle: 'lindsaywalton',
-    role: 'Front-end Developer',
-    imageUrl:
-      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-]
+
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ')
--- pages/patient/demographics.js
+++ pages/patient/demographics.js
@@ -1,11 +1,33 @@
 import PatientContainer from "/components/PatientContainer"
 import PatientTabs from "/components/PatientTabs"
 
+const demos = {
+    fields: {
+        Phone: '(555) 123-4567',
+        Email: 'ricardocooper@example.com',
+        Title: 'Senior Front-End Developer',
+        Team: 'Product Development',
+        Location: 'San Francisco',
+        Sits: 'Oasis, 4th floor',
+        Salary: '$145,000',
+        Birthday: 'June 8, 1990',
+    },
+}
+
 export default function Summary() {
     return (
         <PatientContainer>
             <PatientTabs></PatientTabs>
-            <h1>demographics</h1>
+            <div className="mt-6 max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+                <dl className="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-2">
+                    {Object.keys(demos.fields).map((field) => (
+                        <div key={field} className="sm:col-span-1">
+                            <dt className="text-sm font-medium text-gray-500">{field}</dt>
+                            <dd className="mt-1 text-sm text-gray-900">{demos.fields[field]}</dd>
+                        </div>
+                    ))}
+                </dl>
+            </div>
         </PatientContainer>
     );
 }
